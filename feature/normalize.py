3a4e9fa9910542892987917d3abbd0c2dbeb797a
Hack Chyson <chyson@aliyun.com>
2019-05-28 22:54:04 +0800

    normalize

---
+++ feature/normalize.py
@@ -0,0 +1,8 @@
+from sklearn.preprocessing import Normalizer
+
+data = [[-1, 2], [-0.5, 6], [0, 10], [1, 18]]
+print('data: ', data)
+normalizer = Normalizer()
+normalizer.fit(data)
+print(normalizer.transform(data))
+print(normalizer.transform([[2, 2]]))
